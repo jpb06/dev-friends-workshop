f01cfeca33058a3ef8269066cf6c7912e9f3307f
jpb06 <jp.bois.06@outlook.fr>
2022-04-16 17:44:53 +0200

    using proper type

--- src/tests/msw/handlers/applyHandlerToServer.ts
+++ src/tests/msw/handlers/applyHandlerToServer.ts
@@ -1,11 +1,12 @@
 import { DefaultRequestBody, MockedRequest, RestHandler } from 'msw';
+import { SetupServerApi } from 'msw/lib/types/node';
 
 export const applyHandlerToServer = (
   handler: RestHandler<MockedRequest<DefaultRequestBody>>,
   useServer: boolean
 ) => {
   if (useServer) {
-    const { server } = require('./../server');
+    const { server }: { server: SetupServerApi } = require('./../server');
     return server.use(handler);
   }
 
