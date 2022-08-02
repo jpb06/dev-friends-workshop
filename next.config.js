b747f96d40af040112a63ac9f2dd28ddc5013810
jpb06 <jp.bois.06@outlook.fr>
2022-08-02 18:06:01 +0200

    chore: setting dist folder as build output

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  sassOptions: {
-    includePaths: [
-      path.join(__dirname, 'styles'),
-      path.join(__dirname, 'src', 'components'),
-    ],
-  },
   pageExtensions: ['page.tsx'],
+  distDir: 'dist',
 };
